b0c2f5e3ae52072ca5b9e94bcea64f7a3285f492
Dmitriy <48615297+DimaValetsky@users.noreply.github.com>
2019-04-18 10:56:02 +0300

    Add files via upload

---
+++ work.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+today = datetime.today()
+if today == 'Saturday':
+    print('Party!')
+elif today == 'Sunday':
+    print('Recover.')
+else:
+    print('Work, work, work.')
+        
+        
